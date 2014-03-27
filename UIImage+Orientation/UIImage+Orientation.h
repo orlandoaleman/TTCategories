62e39d66f2a95da4541237cd305fc2139d8bc67c
Orlando Aleman Ortiz <contacto@orlandoaleman.com>
2014-03-27 16:46:06 +0000

    UIImage+Orientation

---
+++ UIImage+Orientation/UIImage+Orientation.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Orientation
+//  orlandoaleman.com
+//
+//  Created by Orlando Aleman Ortiz on 19/10/13.
+//  Copyright (c) 2013 Orlando Aleman Ortiz. All rights reserved.
+//
+
+@interface UIImage (Orientation)
+
+/// Set the image orientation to portrait
+- (UIImage *)fixOrientation;
+@end
